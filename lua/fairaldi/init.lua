bde21cf20ca0c3bb4a239adfa48d0b5ad6efb9cf
Filippo Airaldi <filippoairaldi@gmail.com>
2025-01-13 12:22:49 +0100

    small simplification to code

--- lua/fairaldi/init.lua
+++ lua/fairaldi/init.lua
@@ -31,21 +31,16 @@ vim.api.nvim_create_autocmd("TextYankPost", { -- highlight yanked text
 vim.api.nvim_create_autocmd("LspAttach", {
     group = FairaldiGroup,
     callback = function(e)
-        vim.keymap.set(
-            "n",
-            "<leader>f",
-            function() require("conform").format({ async = true, lsp_format = "fallback" }) end,
-            { desc = "Format file (with conform)" }
-        )
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = e.buf, desc = "Go to definition" })
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = e.buf, desc = "Display hover details" })
-        vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, { buffer = e.buf }) -- ?
-        vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, { buffer = e.buf })     -- ?
-        vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, { buffer = e.buf })
-        vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, { buffer = e.buf, desc = "Find references" })
-        vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, { buffer = e.buf, desc = "Rename symbol" })
-        vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, { buffer = e.buf }) -- ?
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_next,{ buffer = e.buf, desc = "Go to next diagnostic" })
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, { buffer = e.buf, desc = "Go to previous diagnostic" })
+		b = e.buf
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = b, desc = "Go to definition" })
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = b, desc = "Display hover details" })
+        vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, { buffer = b }) -- ?
+        vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, { buffer = b })     -- ?
+        vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, { buffer = b })
+        vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, { buffer = b, desc = "Find references" })
+        vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, { buffer = b, desc = "Rename symbol" })
+        vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, { buffer = b }) -- ?
+        vim.keymap.set("n", "[d", vim.diagnostic.goto_next, { buffer = b, desc = "Go to next diagnostic" })
+        vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, { buffer = b, desc = "Go to previous diagnostic" })
     end
 })
