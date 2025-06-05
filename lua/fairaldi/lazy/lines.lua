8ec96685d9c6836ae5535d01809f42c1405953dd
Filippo Airaldi <filippoairaldi@gmail.com>
2025-06-05 20:14:23 +0200

    added buffer movements

--- lua/fairaldi/lazy/lines.lua
+++ lua/fairaldi/lazy/lines.lua
@@ -8,6 +8,13 @@ return {
         "akinsho/bufferline.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
         opts = {},
+        keys = {
+            { "<Tab>", "<cmd>BufferLineCycleNext<cr>", desc = "Move to next buffer" },
+            { "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", desc = "Move to previous buffer" },
+            { "<leader><", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer left" },
+            { "<leader>>", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer right" },
+            { "<leader>bd", "<cmd>bdelete<cr>", desc = "Close current buffer" },
+        },
     },
 }
 
